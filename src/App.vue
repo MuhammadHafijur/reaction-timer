7be064143d3787430d331d6b34658d220983b45e
muhammadhafijur <hafijur.salah@gmail.com>
2023-02-01 13:01:11 +0600

    :sparkles: feat: Challenge -  showing a results component

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,24 @@
   <h1>Nos Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+  <!-- <p v-if="showResults">Reaction time: {{ score }} ms</p> -->
+  <Results v-if="showResultTwo" :score="score" />
 </template>
 
 <script>
 import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
       delay: null,
       score: null,
-      showResults: false
+      showResults: false,
+      showResultTwo: false
     }
   },
   methods: {
@@ -24,11 +27,13 @@ export default {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true;
       this.showResults = false
+      this.showResultTwo = false
     },
     endGame(reactionTime) {
       this.score = reactionTime
       this.isPlaying = false
       this.showResults = true
+      this.showResultTwo = true
     }
   }
 }
---
+++ src/components/Results.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Your reaction time {{ score }} ms</p>
+    </div>
+</template>
+
+<script>
+ export default {
+    props: ["score"],
+    data(){
+        return {}
+
+    }
+ }
+</script>
